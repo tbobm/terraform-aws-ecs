34b2c88348a9bd6538cb614eb2db5de4c2f61d5f
Theo Bob Massard <tbobm@protonmail.com>
2021-10-25 09:48:45 +0200

    chore(example): specify AWS provider region
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- examples/main.tf
+++ examples/main.tf
@@ -10,6 +10,15 @@ module "ecr" {
   }
 }
 
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS Region used to configure the AWS provider."
+}
+
 variable "vpc" {
   type        = map(string)
   description = "The AWS VPC configuration in which the ECS tasks will run."
